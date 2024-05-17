f2f75f51f70ebe6a16f7f7e862d7374d7641e8b1
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-05-17 09:48:28 +0300

    User Class (Not Completed)

--- Models/User.cs
+++ Models/User.cs
@@ -6,8 +6,8 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public List<Score> Scores { get; set; }
     }
