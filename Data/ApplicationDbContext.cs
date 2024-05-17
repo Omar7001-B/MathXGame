2b5a8972ea6cbb73b61f4a203ad2c332d787b99d
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-05-17 09:49:41 +0300

    DbContext Class

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MathXGame.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MathXGame.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Score> Scores { get; set; }
+    }
+}
