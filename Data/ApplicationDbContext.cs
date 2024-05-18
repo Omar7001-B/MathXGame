1b9d924d97cc56baff3807be680702c0164e0e1e
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-05-18 12:32:34 +0300

    DataBase Relationship One To Many

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,25 @@ namespace MathXGame.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
-        public DbSet<Score> Scores { get; set; }
+        public DbSet<Challenge> Challenges { get; set; }
+        public DbSet<Problem> Problems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define the relationships
+
+            // One-to-Many relationship between User and ChallengeSelectItem
+            modelBuilder.Entity<Challenge>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Challenges)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict); // Prevent cascading delete
+
+            // One-to-Many relationship between ChallengeSelectItem and Problem
+            modelBuilder.Entity<Problem>()
+                .HasOne(p => p.Challenge)
+                .WithMany(c => c.Problems)
+                .HasForeignKey(p => p.ChallengeId)
+                .OnDelete(DeleteBehavior.Cascade); // Cascade delete if a challenge is deleted
+        }
     }
 }
